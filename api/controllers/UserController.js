8a564483ff6eaaf24328eb8eaf033267eff05225
jiteshdugar <jitesh.com@gmail.com>
2015-07-31 09:55:44 +0530

    Revert "Revert "removed upload code""
    This reverts commit 7dc7aef9fe12c0190b989c43664066765a7c7152.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,13 +9,3 @@ module.exports = {
 	
 };
 
-uploadFile: function (req, res) {
-    req.file('avatar').upload({
-      adapter: require('skipper-gridfs'),
-      uri: 'mongodb://[communito:c0mmun!t0@]ds031812.mongolab.com:31812/communito'
-    }, function (err, filesUploaded) {
-      if (err) return res.negotiate(err);
-      return res.ok();
-    });
-}
-
