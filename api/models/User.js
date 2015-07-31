8f55ab3a0276453dd0704f2f662e6cf6b9e17575
jiteshdugar <jitesh.com@gmail.com>
2015-07-31 09:56:56 +0530

    Revert "Included basic attributes"
    This reverts commit 96326e201a18f0b7e107a58e8b7f079e3afa060c.

--- api/models/User.js
+++ api/models/User.js
@@ -8,20 +8,7 @@
 module.exports = {
 
   attributes: {
-	name:{
-        type:"string",
-        required:true,
-        minLength: 2
-	  },      
-	  email:{
-	    type:"email",
-	    required:"true",
-	    unique: true
-	  },
-	  phone:{
-	    type:"string",
-	    required:true
-	  }
+
   }
 };
 
