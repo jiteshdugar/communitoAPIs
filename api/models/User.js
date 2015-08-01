d460fac62eef51791e3656ecf68d52e9bbc8d912
jiteshdugar <jitesh.com@gmail.com>
2015-08-01 20:47:37 +0530

    Updated model with validations

--- api/models/User.js
+++ api/models/User.js
@@ -8,21 +8,52 @@
 module.exports = {
 
   attributes: {
+  	  username:{
+        type:"string", 
+        required:true,
+        minLength: 2,
+        unique: true
+      },
 	  name:{
         type:"string", 
         required:true,
         minLength: 2
       },
+      gender:{
+        type:"string", 
+        required:true,
+        minLength: 1
+      },
       phone:{
         type:"string",
         required:false,
-        unique: true
+        unique: false
       },
       email:{
         type:"email",
-        required:true,
-        unique: true
+        required:false,
+        unique: false
+      },
+      city:{
+        type:"string",
+        required:false
       }
+      hometown:{
+        type:"string",
+        required:false
+      }      
+      country:{
+        type:"string",
+        required:false
+      }
+      language:{
+        type:"string",
+        required:false
+      }  
+      birthdate:{
+      	type:"string",
+      	required:false
+      }          
   }
 };
 
